c7cb01824c9056c4155e0988c3524259fe13e52d
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-10-08 10:17:34 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -3,11 +3,11 @@ alert(1+1);
 alert(Math.sqrt(1+1));
 
 alert(Math.sqrt(2+3));
-alert(1/0)
-alert(0/0)
+alert(1/0);
+alert(0/0);
 
-alret(2+3)
-alert('2'+'3')
-alert(Number('2')+Number('3'))
-alert('2'+3)
-alert(Number('2')+3)
+alret(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
