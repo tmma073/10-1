64098de1c990fd006936c653f98bc883739a0c81
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-10-22 10:15:01 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -3,7 +3,8 @@ function sayhello65(){alert('Hello,'+who65+'!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
 
-var who66='world';
-function sayhello66(){alert(who66+'が来た')}
-function taro66(){who66='太郎';}
-function hanako66(){who66='花子';}
+var taro66='が来た';
+function sayhello66(){alert('Hello,'+who65+'!');}
+function taro66(){alert(who66='太郎');}
+var hanako66='が来た';
+function hanako66(){alert(who66='花子';}
