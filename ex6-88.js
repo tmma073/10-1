161b58c9a89dff276da34f0de3decf19386da47c
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-11-19 09:29:45 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,2 +1,2 @@
 function gazo688(){document.getElementById('who688').innerHTML=
-'<img src="./image/https://github.com/tmma073/10-1/blob/main/image/" alt="オン">';}
+'<img src="./image/bulbon.gif" alt="オン">';}
