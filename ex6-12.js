71c04921f1a36dc5087071e40d3c76ae77e85c59
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-11-26 10:15:44 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -5,7 +5,7 @@ var str = "一文字ずつ表示します。";
 var cnt = document.timer.moji.value.length;
 //文字が全部表示されているか確認
 
-if (cnt < 11) {
+if ( cnt < 11 ) {
 //現在より1文字多く切り出して表示
 document.timer.moji.value = str.substr(0,cnt+1);}
 else{
