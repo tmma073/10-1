02db3c3c34f272507069846567c9792ef05b7285
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-03 10:23:44 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,4 @@
+<--
 function mes() imgchange5(){document.getElementById('logo').src ='./image/0_20140709155407.jpg';}
+--!>
 function mes() imgchange6(){document.getElementById('logo').src ='./image/11483cbadd346c4eb5c3f49e9e2fc682.webp';}
