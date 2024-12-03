ba641c9e6bcf7619997ea4f4a6915d0bd9694bf4
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-03 09:49:19 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,2 @@
 function imgchange1(){document.getElementById('logo').src ='./image/0_20140709155407.jpg';}
+function imgchange2(){document.getElementById('logo').src ='./image/11483cbadd346c4eb5c3f49e9e2fc682.webp';}
