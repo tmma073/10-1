b2df5bacee1e39dd916ffe52946124e4d20c4559
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-03 09:49:52 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,1 +1,2 @@
-function imgchange2(){document.getElementById('logo').src ='./image/0_20140709155407.jpg';}
+function imgchange3(){document.getElementById('logo').src ='./image/0_20140709155407.jpg';}
+function imgchange4(){document.getElementById('logo').src ='./image/';}
