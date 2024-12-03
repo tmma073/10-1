45852b7ffe3fa71ed20f597c38df5900e71201b0
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-03 09:53:33 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo').src ='./image/';}
+function imgchange3(){document.getElementById('logo').src ='./image/masukotto_jokoma02.webp';}
 function imgchange4(){document.getElementById('logo').src ='./image/OIP.jpg';}
