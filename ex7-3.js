f3a0fb4a4b3c0f7906b59f9f06c12b4af23bdf7b
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-10 10:45:08 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,5 +1,10 @@
 function BMI(){
-let x =parseFloat(document.getElementById('input3').value);
-let y =parseFloat(document.getElementById('input4').value);
-let result = x / y;
+let weight =parseFloat(document.getElementById('input3').value);
+let height =parseFloat(document.getElementById('input4').value);
+
+if (isNaN(weight) || isNaN(height) || height <=0){
+alert("正しい数値を入力してください");
+return;}
+
+
 alert(x/y;)}
