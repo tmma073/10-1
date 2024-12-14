731b01c7e98a7fb50ee3f9b62180cc3230cd2824
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-14 11:11:39 +0000

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,8 +1,8 @@
-function BMI(){
+function BMI() {
 let weight =parseFloat(document.getElementById('input3').value);
 let height =parseFloat(document.getElementById('input4').value);
 
-if (isNaN(weight) || isNaN(height) || height <=0){
+if (isNaN(weight) || isNaN(height) || height <= 0) {
 alert("正しい数値を入力してください");
 return;}
 
