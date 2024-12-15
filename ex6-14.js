9c91223bb95bcfe28f463ddc51c77327bebb3201
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-15 01:19:35 +0000

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,5 +1,9 @@
-function imgchange5() { document.getElementById("lOgo").scr = "./image/0_20140709155407.jpg";
+function imgchange5() { 
+let img = document.getElementById("lOgo")
+img.scr = "./image/0_20140709155407.jpg";
 
 //3秒後に元の画像に戻す
-setTimeout(function() { document.getElementById("lOgo").scr = "./image/11483cbadd346c4eb5c3f49e9e2fc682.webp";
-}, 3000);}
+setTimeout(function() { 
+img.src = "./image/11483cbadd346c4eb5c3f49e9e2fc682.webp";
+}, 3000);
+}
