ce66cbfb6b752fbaab4570f0f0af76e2ff4200bb
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-15 10:13:56 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -2,14 +2,14 @@ function BMI() {
 let weight =parseFloat(document.getElementById('input3').value);
 let height =parseFloat(document.getElementById('input4').value);
 
-//入力値のバリエーション
-if (isNaN(weight) || isNaN(height) || height <= 0 || weight <= 0) {
+//入力値の検証
+if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
 alert("正しい数値を入力してください");
 return;}
 
-//BMIの計算
+//BMIの計算(身長はcmからmに変換)
 let bmi = weight / ((height / 100) * (height / 100));
 
 //結果の表示
-document.getElementById('bmi-result').innerText = 
-"BMIは" + bmi.toFixed(2) + "です";}
+alert("BMIは" + bmi.toFixed(2) + "です");
+}
