b6b4009d3f3fc8f5a97b8f2d285d76352120793f
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-15 02:02:20 +0000

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,5 +1,5 @@
 function  nazo2(){
-if (document.getElementById('answer').value == '夢'){
+if (document.getElementById('answer2').value == '夢'){
 alert('あたり!'); //正解の場合 }
 else {
 alert('はずれ'); //不正解の場合 }
