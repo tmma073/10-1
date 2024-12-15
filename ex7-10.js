a18c7af412cdcbdfc4cdc6d9b374f89141dd90b2
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-15 04:13:43 +0000

    Create ex7-10.js

---
+++ ex7-10.js
@@ -0,0 +1,18 @@
+function BMI2() {
+var weight = parseFloat(document.getElementById('input5').value);
+var height = parseFloat(document.getElementById('input6').value) / 100; // cmをmに変換
+var bmi = weight / (height * height);
+var resultText;
+
+if (bmi < 18.5) {
+resultText = '低体重';
+} else {
+if (bmi < 25) {
+resultText = '普通';
+} else {
+resultText = '肥満';
+}
+}
+
+alert('BMI: ' + bmi.toFixed(2) + ' - ' + resultText);
+}
