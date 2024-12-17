549e04fbd3a51082e1c227bc08825280d35ae8c7
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-17 20:01:55 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,2 +1,17 @@
 function toshi1() {
 var x = document.getElementById('toshi1').value;
+var y;
+if (x % 4 === 0) {
+if (x % 100 === 0) {
+if (x % 400 === 0) {
+y = x + '年は閏年'; //400で割り切れる場合
+} else {
+y = x + '年は平年'; //100で割り切れて400で割り切れない場合
+} else {
+y = x + '年は閏年'; //4で割り切れて100で割り切れない場合
+} else {
+y = x + '年は平年'; //4で割り切れない場合
+}
+alert(y);
+
+  
