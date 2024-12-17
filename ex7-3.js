fb9eac43aa5b4b2192bc5e69dde0374d73bb49a3
tmma073 <167832754+tmma073@users.noreply.github.com>
2024-12-17 10:00:28 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -4,12 +4,12 @@ var height =parseFloat(document.getElementById('input4').value);
 
 //入力値の検証
 if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-alert("正しい数値を入力してください");
+alert('正しい数値を入力してください');
 return;}
 
 //BMIの計算(身長はcmからmに変換)
 var bmi = weight / ((height / 100) * (height / 100));
 
 //結果の表示
-alert("BMIは" + bmi.toFixed(2) + "です。");
+alert('BMIは' + bmi.toFixed(2) + 'です。');
 }
